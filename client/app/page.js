632cb4cc49e564bdeb88e87a8fd03a36ce0416a3
Robertobolla9 <robertobolla9>
2025-01-15 06:29:00 -0300

    se agrego la metada

--- client/app/page.js
+++ client/app/page.js
@@ -1,6 +1,15 @@
 import Hero from "./_components/Hero";
 import ProductSection from "./_components/ProductSection";
 
+export const metadata = {
+  title: "Templates for Developers - EscaleDevs",
+  description:
+    "Clearly describes the products you offer and emphasizes the technologies: React, Next.js, and WordPress plugins.",
+  icons: {
+    icon: "/favicon.png", // Cambia la ruta si el favicon tiene otro formato o nombre
+  },
+};
+
 export default function Home() {
   return (
     <div>
