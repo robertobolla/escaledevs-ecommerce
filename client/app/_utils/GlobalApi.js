8ec8398d98ecad53762b53d255c264df3364d856
Robertobolla9 <robertobolla9>
2025-01-17 01:11:35 -0300

    solucion cors definitiva2

--- client/app/_utils/GlobalApi.js
+++ client/app/_utils/GlobalApi.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = "http://api.escaledevs.com";
+const apiUrl = "http://api.escaledevs.com/api";
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
