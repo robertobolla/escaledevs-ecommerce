899ea782203e8623267972e82e0515f2a8d952c9
Robertobolla9 <robertobolla9>
2025-01-17 04:13:46 -0300

    solucion cors api2

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -15,14 +15,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // Redirige todas las solicitudes a /api/
-        destination: "http://api.escaledevs.com/api/:path*", // Redirige a tu backend HTTP
-      },
-    ];
-  },
 };
 
 export default nextConfig;
